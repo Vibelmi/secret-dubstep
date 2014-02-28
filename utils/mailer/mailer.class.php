ad8f0e71e3d4a887862154821ddfe4465834f3fa
Vibelmi <vibelmi1@gmail.com>
2014-02-28 17:08:59 +0100

    mailer class

---
+++ utils/mailer/mailer.class.php
@@ -0,0 +1,17 @@
+<?php
+/*
+* This object can be used to send mails to the users in our database
+*/
+class mailer{
+
+	function __construct(){
+	
+	}
+	function sendSingleMail(){
+	
+	}
+	function sendMultiMail(){
+	
+	}
+	function send
+}
