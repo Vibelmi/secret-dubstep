e8757caa1ed183c75bff2dc9a14cd8a1822ea7f0
ipshal <ipshal@ipshal-debian>
2014-03-04 18:52:59 +0100

    Added the variable $URL to see the current url

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
   include_once("utils/includes.php");
   //Create the object that creates the page
   $pageBuilder = new pageBuilder();
+  //The current url
+  $URL = $_SERVER['REQUEST_URI'];
   //Print the requested page, pages are defined in the file pages.xml. 
   //If the page is not found, load the first page in pages.xml (usually the main).
   $page = "";
