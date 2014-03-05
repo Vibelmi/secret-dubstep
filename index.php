d9a9353c06f45cce7eb21062fb43f71d17930271
user <admin@local.host>
2014-03-05 21:26:28 +0100

    Merge origin/master
    Conflicts:
    	classes/pageBuilder.class.php
    	modules/pages.xml

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 
   //Use this file for all the includes!!
   include_once("utils/includes.php");
+  
+  //This is the global variable to use Smarty. You can call it as $GLOBALS['smarty']
+  $smarty = new Smarty();
+  $smarty->setTemplateDir('views/templates');
+  $smarty->setCompileDir('views/templates_c');
+  
+  //The current url
+  $URL = $_SERVER['REQUEST_URI'];
+  
   //Create the object that creates the page
   $pageBuilder = new pageBuilder();
+  
   //Print the requested page, pages are defined in the file pages.xml. 
   //If the page is not found, load the first page in pages.xml (usually the main).
-  $page = "";
-  if(isset($GLOBALS['CLEANED_GET']["page"])){ //Get langunage
-	$page = $GLOBALS['CLEANED_GET']["page"];
-  }
-  $pageBuilder->printPage($page);
+
+  if(isset($GLOBALS['CLEANED_POST']["ajax"])){
+      $pageBuilder->printModule($GLOBALS['CLEANED_POST']["ajax"]);
+  }else{
+    $page = "";
+    if (isset($GLOBALS['CLEANED_GET']["page"])) { //Get langunage
+        $page = $GLOBALS['CLEANED_GET']["page"];
+    }
+    $pageBuilder->printPage($page);
+}
+  
 ?>
