de4ebdcfbbfc0f025dc482405e3059498580c628
user <admin@local.host>
2014-03-06 03:44:21 +0100

    Code formated

--- modules/login/controller/login.php
+++ modules/login/controller/login.php
@@ -1,23 +1,26 @@
 <?php
-include_once ("modules/module/controller/module.php");
-class login extends module{
-	
-	function __construct($fPath = "modules/login/data/content.xml") {
-		$this->contentFilePath = $fPath;
-		$this->position = "header";
-		$this->display = true;
-		$this->content = $this->getContentByLang($GLOBALS['language']);
-	}
 
-	public function printContent(){
-		ob_start();
-                //$cont=$this->content;
-		include("modules/login/controller/index.php");
-		$returned = ob_get_contents();
-		ob_end_clean();
-		$returned .= $this->content->name ." ".$this->content->age;
-		return $returned;
-	}
+include_once ("modules/module/controller/module.php");
+
+class login extends module {
+
+    function __construct($fPath = "modules/login/data/content.xml") {
+        $this->contentFilePath = $fPath;
+        $this->position = "header";
+        $this->display = true;
+        $this->content = $this->getContentByLang($GLOBALS['language']);
+    }
+
+    public function printContent() {
+        ob_start();
+        //$cont=$this->content;
+        include("modules/login/controller/index.php");
+        $returned = ob_get_contents();
+        ob_end_clean();
+        $returned .= $this->content->name . " " . $this->content->age;
+        return $returned;
+    }
 
 }
+
 ?>
