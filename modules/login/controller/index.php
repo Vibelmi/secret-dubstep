bbcd31722c1f6246c3861954da686983c4020319
ignasori <ignasori94@gmail.com>
2014-03-07 02:42:22 +0100

    Reestructure code

--- modules/login/controller/index.php
+++ modules/login/controller/index.php
@@ -1,11 +1,8 @@
 <?php
 
 if (isset($GLOBALS['CLEANED_POST']["email"]) && isset($GLOBALS['CLEANED_POST']["pass"]) && isset($GLOBALS['CLEANED_POST']["token"])) {
-    if ($GLOBALS['CLEANED_POST']["token"] === "0") { //user
-        include("modules/login/model/checkuser.php");
-    } else { //provider
-        include("modules/login/model/checkprovider.php");
-    }
+        include("modules/login/model/validateDAO.php");
+        validateDAO::getInstance()->validate($GLOBALS['CLEANED_POST']["token"]);
 } else {
     $cont = $this->content;
     include("modules/login/view/main.php");
