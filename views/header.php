9cd8973ed66b741e6e1a02354e221c8362a54db7
usuari <usuari@kickseed>
2014-03-10 17:10:41 +0100

    Insert Jquery and Jquery ui

--- views/header.php
+++ views/header.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<script src="js/jquery-2.1.0.min.js"></script>  
+<script src="js/jquery-ui.min.js"></script>    
 <script src="utils/includejs.js"></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width">
