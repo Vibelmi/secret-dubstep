e49337132e848d4ec35ad21b6cd8ea16c9926aee
ignasori <ignasori94@gmail.com>
2014-03-11 17:58:03 +0100

    Fixed clean inputs to don't clean JSONs

--- utils/cleanInput.php
+++ utils/cleanInput.php
@@ -1,32 +1,37 @@
 <?php
+
 /*
-* Here all inputs will be cleaned of not appropriate content
-* Only string inputs are validated and cleaned.
-*/
+ * Here all inputs will be cleaned of not appropriate content
+ * Only string inputs are validated and cleaned.
+ */
 //THIS TWO ARE THE VARIABLES THAT WE MUST USE IN ALL THE APPLICATION.
 $CLEANED_GET = $_GET;
 $CLEANED_POST = $_POST;
 
-
-function cleanInput($input){
+function cleanInput($input) {
     return htmlspecialchars(addslashes(stripslashes(strip_tags(trim($input)))));
 }
 
-function cleanGET(){
-  global $CLEANED_GET;
-  foreach ($CLEANED_GET as $key => $value) { 
-      if(gettype($value) == "string"){
-          $CLEANED_GET[$key] = cleanInput($value);
-      }
-  }
+function cleanGET() {
+    global $CLEANED_GET;
+    foreach ($CLEANED_GET as $key => $value) {
+        if (gettype($value) == "string") {
+            if (!json_decode($value)) {
+                $CLEANED_GET[$key] = cleanInput($value);
+            }
+        }
+    }
 }
-function cleanPOST(){
-  global $CLEANED_POST;
-  foreach ($CLEANED_POST as $key => $value) { 
-      if(gettype($value) == "string"){
-          $CLEANED_POST[$key] = cleanInput($value);
-      }
-  }
+
+function cleanPOST() {
+    global $CLEANED_POST;
+    foreach ($CLEANED_POST as $key => $value) {
+        if (gettype($value) == "string") {
+            if (!json_decode($value)) {
+                $CLEANED_POST[$key] = cleanInput($value);
+            }
+        }
+    }
 }
 
 cleanGET();
