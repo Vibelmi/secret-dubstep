77c4fca39247e5d5fb422d87b39dfb042bc30bd1
ipshal <ipshal@ipshal-debian>
2014-03-12 00:11:42 +0100

    First version of the list tickets modules. Now can list the tickets of the guests and their response

---
+++ modules/list_tickets/controller/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$cont = $this->content;
+if (isset($_SESSION['email'])) {
+    if ((isset($GLOBALS['CLEANED_GET']["idtu"])) && ($_SESSION['token'] == 0)) {
+        include("modules/list_tickets/model/userticket.php");
+    } elseif ((isset($GLOBALS['CLEANED_GET']["idtp"])) && ($_SESSION['token'] == 1)) {
+        include("modules/list_tickets/model/provticket.php");
+    } elseif ($_SESSION['token'] == 0) {
+        include("modules/list_tickets/view/mainuser.php");
+    } elseif ($_SESSION['token'] == 1) {
+        include("modules/list_tickets/view/mainprov.php");
+    } elseif ($_SESSION['token'] == 10) {
+        include("modules/list_tickets/view/mainadmin.php");
+    }
+} elseif (isset($GLOBALS['CLEANED_GET']["idtg"], $GLOBALS['CLEANED_POST']["passwordt"])) {
+    include("modules/list_tickets/model/guesttickets.php");
+} elseif (isset($GLOBALS['CLEANED_GET']["idtg"])) {
+    include("modules/list_tickets/view/mainguest.php");
+} else {
+    //fail
+}
