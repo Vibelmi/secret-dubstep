7813238740663a1f6c7bb33a7b6742120daf72a1
daniel <daniel@daniel-P35-S3G>
2014-03-12 04:15:20 +0100

    Small changes on polls module.

--- modules/polls/view/results_poll.php
+++ modules/polls/view/results_poll.php
@@ -3,33 +3,36 @@ include_once("modules/polls/model/polls_BLL.php");
 $polls_inst = polls_BLL::getInstance();
 $list_polls = $polls_inst->list_polls();
 ?>   
-<div id="polls_list">
-    <?php
-    foreach ($list_polls[0] as $key => $poll) {
-        if ($poll['active']) {
+<div class="mod">
+    <div class="module_div_title"><h2>POLL RESULTS</h2></div>
+    <div id="polls_list" class="module_div_content">
+        <?php
+        foreach ($list_polls[0] as $key => $poll) {
+            if ($poll['active']) {
 
-            echo '<h4>' . $poll['title'] . '</h4>';
-            echo '<div>';
-            echo '<ul>';
-            foreach ($list_polls[1] as $key => $option) {
-                if ($option['poll_id'] === $poll['poll_id']) {
-                    ?>
-                    <li>
+                echo '<h4>' . $poll['title'] . '</h4>';
+                echo '<div>';
+                echo '<ul>';
+                foreach ($list_polls[1] as $key => $option) {
+                    if ($option['poll_id'] === $poll['poll_id']) {
+                        ?>
+                        <li>
+                            <?php
+                            echo $option['option_text'] . ' ';
+                            $answer = $polls_inst->count_votes($option['option_id']);
+                            echo '[ ' . $answer['count'] . ' ' . $cont->votes . ' ]';
+                            ?>        
+
+                        </li>                    
                         <?php
-                        echo $option['option_text'] . ' ';
-                        $answer = $polls_inst->count_votes($option['option_id']);
-                        echo '[ ' . $answer['count'] . ' ' . $cont->votes . ' ]';
-                        ?>        
-
-                    </li>                    
-                    <?php
+                    }
                 }
-            }
-            echo '</ul>';
-            echo '</div>';
-        };
-    }
-    ?>
+                echo '</ul>';
+                echo '</div>';
+            };
+        }
+        ?>
+    </div>
 </div>
 
 <script src="modules/polls/js/show_poll_events.js"></script>
--- modules/polls/view/show_poll.php
+++ modules/polls/view/show_poll.php
@@ -6,33 +6,36 @@ if (isset($GLOBALS['CLEANED_POST']["option_id"])) {
     $polls_inst->vote($GLOBALS['CLEANED_POST']["option_id"]);
     //SELECT * FROM polls INNER JOIN polls_options ON polls.poll_id=polls_options.poll_id INNER JOIN polls_answers ON polls_options.option_id=polls_answers.option_id
     include_once("modules/polls/view/results_poll.php");
-    
 } else {
     $list_polls = $polls_inst->list_polls();
     ?>   
-    <div id="polls_list">
-        <?php
-        foreach ($list_polls[0] as $key => $poll) {
-            if ($poll['active']) {
+    <div class="mod">
 
-                echo '<h4>' . $poll['title'] . '</h4>';
-                echo '<div>';
-                echo '<ul>';
-                foreach ($list_polls[1] as $key => $option) {
-                    if ($option['poll_id'] === $poll['poll_id']) {
-                        ?>
-                        <li>
-                            <input type="radio" name="option" class="option" value="<?php echo $option['option_id'] ?>"><?php echo $option['option_text'] ?><br>
-                        </li>                    
-                        <?php
+    <div class="module_div_title"><h2>POLL</h2></div>
+        <div id="polls_list" class="module_div_content">
+            <?php
+            foreach ($list_polls[0] as $key => $poll) {
+                if ($poll['active']) {
+
+                    echo '<h4>' . $poll['title'] . '</h4>';
+                    echo '<div>';
+                    echo '<ul>';
+                    foreach ($list_polls[1] as $key => $option) {
+                        if ($option['poll_id'] === $poll['poll_id']) {
+                            ?>
+                            <li>
+                                <input type="radio" name="option" class="option" value="<?php echo $option['option_id'] ?>"><?php echo $option['option_text'] ?><br>
+                            </li>                    
+                            <?php
+                        }
                     }
-                }
-                echo '</ul>';
-                echo '</div>';
-            };
-        }
-        ?>
-    </div>	 
+                    echo '</ul>';
+                    echo '</div>';
+                };
+            }
+            ?>
+        </div>
+    </div>
     <?php
 }
 ?>
