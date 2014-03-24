73ddfb59f81fae1a1356d6a08b768f680d7aa852
Vibelmi <vibelmi1@gmail.com>
2014-03-24 19:03:01 +0100

    Added language menu

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
-<footer><small>www.daw2.bocairent.net</small></footer>
+<footer><small>www.daw2.bocairent.net</small><nav class="languages"><a href="">es</a> | <a href="">en</a></nav></footer>
 </div>
 
 </div>
